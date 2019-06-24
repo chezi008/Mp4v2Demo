610455d612fc29b44e619491b192cf7a5ceb27a1
Neo <neo@cctips.com>
2019-06-24 22:07:55 +0800

    move jni to middle library & use dependencies

--- middlelibrary/src/main/java/com/seuic/jni/Mp4v2Helper.java
+++ middlelibrary/src/main/java/com/seuic/jni/Mp4v2Helper.java
@@ -3,14 +3,12 @@ package com.seuic.jni;
 import android.util.Log;
 
 /**
- * 描述：
- * 作者：chezi008 on 2017/4/12 16:14
- * 邮箱：chezi008@163.com
+ * 描述： 作者：chezi008 on 2017/4/12 16:14 邮箱：chezi008@163.com
  */
 
 public class Mp4v2Helper {
 
-    public static native int initMp4Encoder(String outputFilePath,int width,int height);
+    public static native int initMp4Encoder(String outputFilePath, int width, int height);
 
     public static native int mp4VEncode(byte[] data, int size);
 
